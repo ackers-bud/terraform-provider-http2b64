8bf361b712e548f58b47ac6b09a7d5a6dc746556
Mark Ackroyd <mark.ackroyd@thisisbud.com>
2022-11-21 16:24:02 +0000

    Update 20221121-162402

--- provider/provider.go
+++ provider/provider.go
@@ -1,31 +1,32 @@
 package provider
 
 import (
-	"context"
+    "context"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+    "github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+    "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func New(version string) func() *schema.Provider {
-	return func() *schema.Provider {
-		p := &schema.Provider{
-			ResourcesMap: map[string]*schema.Resource{
-				"http-b64": resourcehttpb64(),
-			},
-		}
+    return func() *schema.Provider {
+        p := &schema.Provider{
 
-		p.ConfigureContextFunc = configure(version, p)
+            ResourcesMap: map[string]*schema.Resource{
+                "http-b64": resourcehttpb64(),
+            },
+        }
 
-		return p
-	}
+        p.ConfigureContextFunc = configure(version, p)
+
+        return p
+    }
 }
 
 type apiClient struct {
 }
 
 func configure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	return func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		return &apiClient{}, nil
-	}
+    return func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
+        return &apiClient{}, nil
+    }
 }
